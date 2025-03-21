8bf176a06e349efc7ce23e6e82e245ad2945ab95
4min dev <sadgerondo@gmail.com>
2025-03-21 17:29:34 +0300

    finalized: control panel for video

--- src/components/pages/editor/ui/videoPlayerPanel/VideoPlayerPanel.tsx
+++ src/components/pages/editor/ui/videoPlayerPanel/VideoPlayerPanel.tsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState, useRef } from 'react'
 import styles from './VideoPlayerPanel.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExpand, faPause, faPlay, faVolumeHigh, faVolumeMute } from '@fortawesome/free-solid-svg-icons'
+import {
+    faExpand,
+    faPause,
+    faPlay,
+    faVolumeHigh,
+    faVolumeMute,
+} from '@fortawesome/free-solid-svg-icons'
 
 type TVideoPlayerPanel = {
-    isVideoPaused: boolean,
-    playVideo: (videoRef: HTMLVideoElement) => void,
-    pauseVideo: (videoRef: HTMLVideoElement) => void,
-    videoRef: React.RefObject<HTMLVideoElement>,
-    handleFullscreenToggle: () => void,
+    isVideoPaused: boolean
+    playVideo: (videoRef: HTMLVideoElement) => void
+    pauseVideo: (videoRef: HTMLVideoElement) => void
+    videoRef: React.RefObject<HTMLVideoElement>
+    handleFullscreenToggle: () => void
     isFullscreen: boolean
 }
 
-const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVideo, pauseVideo, videoRef, handleFullscreenToggle, isFullscreen }) => {
+const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({
+    isVideoPaused,
+    playVideo,
+    pauseVideo,
+    videoRef,
+    handleFullscreenToggle,
+    isFullscreen,
+}) => {
     const [isMuteHovered, setIsMuteHovered] = useState<boolean>(false)
     const [isMuted, setIsMuted] = useState<boolean>(false)
     const [playBack, setPlayback] = useState({
@@ -24,6 +37,8 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
     const [tooltipTime, setTooltipTime] = useState<string>("00:00")
     const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false)
     const isDraggingRef = useRef<boolean>(false)
+    const [volume, setVolume] = useState(1)
+    const [isDragging, setIsDragging] = useState(false)
 
     const formatTime = (timeInSeconds: number): string => {
         if (isNaN(timeInSeconds)) return "00:00"
@@ -45,12 +60,10 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
         if (videoRef.current && !isDraggingRef.current) {
             const currentTime = videoRef.current.currentTime
             const duration = videoRef.current.duration
-
             setPlayback((prev) => ({
                 ...prev,
                 current: formatTime(currentTime),
             }))
-
             const progressPercentage = (currentTime / duration) * 100
             setProgress(isNaN(progressPercentage) ? 0 : progressPercentage)
         }
@@ -73,81 +86,60 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
 
     const handleTimelineClick = (event: React.MouseEvent<HTMLDivElement>) => {
         if (!videoRef.current) return
-
         const timelineWrapper = event.currentTarget
         const rect = timelineWrapper.getBoundingClientRect()
         let clickPosition = event.clientX - rect.left
-
         clickPosition = Math.max(0, Math.min(rect.width, clickPosition))
-
         const percentage = (clickPosition / rect.width) * 100
-
         const duration = videoRef.current.duration
         const newTime = (percentage / 100) * duration
-
         videoRef.current.currentTime = newTime
     }
 
     const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
         if (!videoRef.current) return
-
         isDraggingRef.current = true
-
         if (!isVideoPaused) {
             pauseVideo(videoRef.current)
         }
-
         const timelineWrapper = event.currentTarget
         const updateProgress = (event: MouseEvent) => {
             const rect = timelineWrapper.getBoundingClientRect()
             let clickPosition = event.clientX - rect.left
-
             clickPosition = Math.max(0, Math.min(rect.width, clickPosition))
-
             const percentage = (clickPosition / rect.width) * 100
-
             const duration = videoRef.current?.duration || 0
             const newTime = (percentage / 100) * duration
-
             if (videoRef.current) {
                 videoRef.current.currentTime = newTime
                 setProgress(percentage)
-
                 setPlayback((prev) => ({
                     ...prev,
                     current: formatTime(newTime),
                 }))
             }
         }
-
         const handleMouseUp = () => {
             isDraggingRef.current = false
             window.removeEventListener('mousemove', updateProgress)
             window.removeEventListener('mouseup', handleMouseUp)
-
             if (!isVideoPaused && videoRef.current) {
                 playVideo(videoRef.current)
             }
         }
-
         window.addEventListener('mousemove', updateProgress)
         window.addEventListener('mouseup', handleMouseUp)
     }
 
     const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
         if (!videoRef.current) return
-
         const timelineWrapper = event.currentTarget
         const rect = timelineWrapper.getBoundingClientRect()
         let clickPosition = event.clientX - rect.left
-
         clickPosition = Math.max(0, Math.min(rect.width, clickPosition))
-
         const percentage = (clickPosition / rect.width) * 100
-
         const duration = videoRef.current.duration
         const newTime = (percentage / 100) * duration
-
         setTooltipPosition({
             x: rect.left + clickPosition,
             y: event.clientY - 30,
@@ -167,13 +159,62 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
         if (videoRef.current) {
             videoRef.current.addEventListener("loadedmetadata", handleLoadedMetadata)
             videoRef.current.addEventListener("timeupdate", handleTimeUpdate)
-
             return () => {
                 videoRef.current?.removeEventListener("loadedmetadata", handleLoadedMetadata)
                 videoRef.current?.removeEventListener("timeupdate", handleTimeUpdate)
             }
         }
     }, [])
+
+    const handleSoundWrapperClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        const wrapper = e.currentTarget
+        const rect = wrapper.getBoundingClientRect()
+        const clickPosition = e.clientX - rect.left
+        const newVolume = Math.min(Math.max(clickPosition / rect.width, 0), 1)
+        setVolume(newVolume)
+    }
+
+    const handleMouseSoundDown = () => {
+        setIsDragging(true)
+    }
+
+    const handleMouseSoundMove = (e: MouseEvent) => {
+        if (!isDragging) return
+
+        const wrapper = document.querySelector(`.${styles.soundLineContainer}`) as HTMLDivElement
+        if (!wrapper) return
+
+        const rect = wrapper.getBoundingClientRect()
+        const clickPosition = e.clientX - rect.left
+        const newVolume = Math.min(Math.max(clickPosition / rect.width , 0), 1)
+
+        setVolume(newVolume)
+    }
+
+    const handleMouseSoundUp = () => {
+        setIsDragging(false)
+    }
+
+    useEffect(() => {
+        if (isDragging) {
+            document.addEventListener('mousemove', handleMouseSoundMove)
+            document.addEventListener('mouseup', handleMouseSoundUp)
+        } else {
+            document.removeEventListener('mousemove', handleMouseSoundMove)
+            document.removeEventListener('mouseup', handleMouseSoundUp)
+        }
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseSoundMove)
+            document.removeEventListener('mouseup', handleMouseSoundUp)
+        }
+    }, [isDragging])
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.volume = volume
+        }
+    }, [volume])
 
     return (
         <div className={`flex column ${styles.videoPlayerPanel}`}>
@@ -189,42 +230,57 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
                     className={styles.videoPlayerTimeline}
                     style={{ width: `${progress}%` }}
                 ></div>
-
                 {isTooltipVisible && (
                     <div
                         className={`${styles.tooltip} ${isFullscreen ? styles.fullScreen : ''}`}
                         style={{
-
                             left: `${tooltipPosition.x}px`,
-
                         }}
                     >
                         {tooltipTime}
                     </div>
                 )}
             </div>
-
             <div className={`flex align__center justify__space__between ${styles.videoPlayerTimelineControls}`}>
                 <div className={`flex align__center ${styles.videoPlayterTimelineControlButtons}`}>
-                    <button type='button' className={styles.playButton} onClick={handlePlaybackButton}>
-                        <FontAwesomeIcon size="2x" icon={isVideoPaused ? faPlay : faPause} color='white' />
+                    <button type="button" className={styles.playButton} onClick={handlePlaybackButton}>
+                        <FontAwesomeIcon size="2x" icon={isVideoPaused ? faPlay : faPause} color="white" />
                     </button>
-
-                    <button
-                        type='button'
-                        className={styles.muteButton}
+                    <div
+                        className={`flex align__center ${styles.muteButtonContainer} ${isMuteHovered ? styles.hovered : ''}`}
                         onMouseEnter={() => setIsMuteHovered(true)}
                         onMouseLeave={() => setIsMuteHovered(false)}
-                        onClick={handleMuteToggle}
                     >
-                        <FontAwesomeIcon size="2x" icon={isMuted ? faVolumeMute : faVolumeHigh} color='white' />
-                    </button>
-
-                    {isMuteHovered ? (
-                        <div className={styles.soundLineContainer}>
-                            <div className={styles.soundLineControlCorner}></div>
+                        <button
+                            type="button"
+                            className={styles.muteButton}
+                            onClick={() => setVolume((prev) => (prev === 0 ? 1 : 0))}
+                        >
+                            <FontAwesomeIcon
+                                size="2x"
+                                icon={volume === 0 ? faVolumeMute : faVolumeHigh}
+                                color="white"
+                            />
+                        </button>
+                        <div
+                            className={styles.soundLineContainer}
+                            onClick={handleSoundWrapperClick}
+                        >
+                            <div
+                                className={styles.soundLineControlThumb}
+                                style={{
+                                    '--volume': volume,
+                                    width: `calc(var(--volume) * 100% - 8px)`,
+                                } as React.CSSProperties}
+                            >
+                                <div
+                                    className={styles.soundLineControlCorner}
+                                    onMouseDown={handleMouseSoundDown}
+                                ></div>
+                            </div>
                         </div>
-                    ) : (
+                    </div>
+                    {!isMuteHovered && (
                         <div className={styles.playbackValueContainer}>
                             <span className={styles.currentPlaybackValue}>{playBack.current}</span>
                             &nbsp;/&nbsp;
@@ -232,14 +288,12 @@ const VideoPlayerPanel: React.FC<TVideoPlayerPanel> = ({ isVideoPaused, playVide
                         </div>
                     )}
                 </div>
-
                 <input
                     className={`flex align__center justify__center ${styles.addCommentInput}`}
-                    placeholder='Add Comment'
+                    placeholder="Add Comment"
                 />
-
-                <button type='button' className={styles.fullscreenButton} onClick={handleFullscreenToggle}>
-                    <FontAwesomeIcon size="2x" icon={faExpand} color='white' />
+                <button type="button" className={styles.fullscreenButton} onClick={handleFullscreenToggle}>
+                    <FontAwesomeIcon size="2x" icon={faExpand} color="white" />
                 </button>
             </div>
         </div>
